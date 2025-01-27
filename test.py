ef7898125c29a13a7664e2e084c702c595f497b0
justdanny0 <153352400+Young-Keun-LEE@users.noreply.github.com>
2025-01-27 14:23:02 +0000

    Beakjoon 2941

---
+++ test.py
@@ -0,0 +1,31 @@
+import sys
+voca = list(input()) #input voca from user
+croatian_word = ('c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=') #dictionary of croatian word
+count_word = 0 #count the number of word
+word = None
+index = 0 #index of array
+
+while index < len(voca):
+    word = voca[index]
+    
+    if index < len(voca) - 1:
+        word = voca[index] + voca[index + 1]
+    
+    if word in croatian_word:
+        index += 2
+        count_word += 1
+        continue
+    
+    if index < len(voca) - 2:
+        word = voca[index] + voca[index + 1] + voca[index + 2]
+    
+    if word in croatian_word:
+        index += 3
+        count_word += 1
+        continue
+    
+    count_word += 1
+    index += 1
+
+
+print(count_word)
