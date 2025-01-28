80ce7cb42a221d86655dda6a331c6f2732d5a545
justdanny0 <153352400+Young-Keun-LEE@users.noreply.github.com>
2025-01-28 14:53:55 +0000

    d

---
+++ greedy_example3.py
@@ -0,0 +1,22 @@
+import sys
+
+N = int(sys.stdin.readline()) # Input the number of city
+length = list(map(int, sys.stdin.readline().split())) # Input length of distance
+price =  list(map(int, sys.stdin.readline().split())) # Input price of oil
+
+def next_destination(index):
+    for j in range(index + 1, N - 1):
+        if price[index] > price[j]:
+            return j
+    return N - 1
+
+i = 0
+result = 0 #Minimum oil price
+while i < N - 1:
+    j = next_destination(i)
+    result += price[i] * sum(length[i:j])
+    i = j
+    
+    
+        
+sys.stdout.write(str(result))
---
+++ string_example.py
@@ -0,0 +1,11 @@
+import sys
+N = int(sys.stdin.readline()) #The number of vocabulary
+string = [] #Save unchecked word
+
+for i in range(N):
+    string.append(sys.stdin.readline().strip())
+    
+for s in string: #Check group word
+    
+    
+
