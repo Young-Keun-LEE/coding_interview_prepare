0645f4e08e9d5e3771532548c35561780de01634
Young-Keun-LEE <justdanny1998@gmail.com>
2025-07-19 16:32:37 +0900

    백준 15650

---
+++ backtracking2.py
@@ -0,0 +1,14 @@
+def backtrack(start):
+
+    if len(path) == M:
+        print(*path)
+        return
+    
+    for i in range(start, N + 1):
+            path.append(i)
+            backtrack(i + 1)
+            path.pop()
+            
+N, M = map(int, input().split(" "))
+path = []
+backtrack(1)
