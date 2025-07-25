21470aeacca1d8ac9e4422c0e0dca4b0a9d18daf
Young-Keun-LEE <justdanny1998@gmail.com>
2025-07-25 22:59:14 +0900

    백준 2579

---
+++ Dp6.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # The number of stair
+stairs = [0]
+
+for _ in range(N):
+    stairs.append(int(input().strip()))
+    
+dp = [0] * (N + 1)
+if N >= 1:
+    dp[1] = stairs[1]
+if N >= 2:
+    dp[2] = stairs[1] + stairs[2]
+if N >= 3:
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+
+
+
+for i in range(4, N + 1):
+    dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i])
+
+print(dp[N])
