befb193fbfd76e1ed037374a98cd65c3af3f555f
Young-Keun-LEE <justdanny1998@gmail.com>
2025-08-01 15:59:48 +0900

    백준 1629 Another approach

--- Divide_conquer1.py
+++ Divide_conquer1.py
@@ -1,13 +1,23 @@
-def fast_pow(a, b, c):
-    if b == 0:
-        return 1
+# def fast_pow(a, b, c):
+#     if b == 0:
+#         return 1
     
-    if b % 2 == 0:
-        return (fast_pow(a, b // 2, c) % c) ** 2 % c
+#     if b % 2 == 0:
+#         return (fast_pow(a, b // 2, c) % c) ** 2 % c
     
-    else:
-        return (fast_pow(a, b // 2, c) % c) ** 2 * a % c
+#     else:
+#         return (fast_pow(a, b // 2, c) % c) ** 2 * a % c
 
 A, B, C = map(int, input().split())
-print(fast_pow(A, B, C))
+# print(fast_pow(A, B, C))
+result = 1
+A %= C
+while B > 0:
+    if B % 2 == 1:
+        result = (result * A) % C
+    A = (A * A) % C
+    B //= 2
+
+print(result)
+
 
